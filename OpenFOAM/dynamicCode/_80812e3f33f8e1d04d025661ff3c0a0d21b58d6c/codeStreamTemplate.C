aaa0ca9cb00dbc5ed38890284cc4a1f52f62dc1b
jamelo14 <joaomelo.ja@gmail.com>
2024-04-30 11:48:53 -0300

    Enhancing mesh quallity and patches.

---
+++ OpenFOAM/dynamicCode/_80812e3f33f8e1d04d025661ff3c0a0d21b58d6c/codeStreamTemplate.C
@@ -0,0 +1,73 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) YEAR OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Description
+    Template for use with codeStream.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dictionary.H"
+#include "Ostream.H"
+#include "Pstream.H"
+#include "unitConversion.H"
+
+//{{{ begin codeInclude
+
+//}}} end codeInclude
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+//{{{ begin localCode
+
+//}}} end localCode
+
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+extern "C"
+{
+    void codeStream_80812e3f33f8e1d04d025661ff3c0a0d21b58d6c
+    (
+        Ostream& os,
+        const dictionary& dict
+    )
+    {
+//{{{ begin code
+        #line 1 "/home/meloo/Documents/Cilindro_termico_new/OpenFOAM/system/blockMeshDict"
+os << (-1.0*(20+100));
+//}}} end code
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
+
